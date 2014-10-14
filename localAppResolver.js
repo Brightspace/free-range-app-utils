81f23fa2a3d8085c1d75d084618dee6319fbc65e
Carl Pacey <Carl.Pacey@d2l.com>
2014-10-14 17:13:09 -0400

    Using the caller's package.json
    Was previously accidentally always using sheepdog's package.json.

--- localAppResolver.js
+++ localAppResolver.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var path = require('path');
+var packageJson = path.join( process.cwd(), 'package.json' );
+
 function LocalAppRegistry(key, opts) {
     opts = opts || {};
-    opts.key = key || require('./package.json').name;
+    opts.key = key || require( packageJson ).name;
     opts.hostname = opts.hostname || 'localhost';
     opts.port = opts.port || 3000;
     opts.dist = opts.dist || 'dist';
